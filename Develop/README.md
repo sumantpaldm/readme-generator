a23bedcfbd0789b83dd3cf1747b08cb4c83dddd3
SINGH 1258 <sumantpaldm@gmail.com>
2021-08-19 05:10:19 -0400

    added license section and assets folder

---
+++ Develop/README.md
@@ -0,0 +1,48 @@
+# SS
+
+  ## Description
+
+  SS
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+  ## Table of Content
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Tests](#tests)
+  * [Contact](#contact)
+
+  ## Installation
+
+  none
+
+  ## Usage
+ 
+  S
+
+  ## Credits
+
+  undefined
+
+
+  ## License
+
+    This application has MIT
+
+  ## Tests
+
+  none
+
+  ## Contact
+  Feel free to reach me out !!!
+  
+  S
+  
+  S
+
+
+
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,147 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is name of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide some description about the project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Specify the installation requirements?',
+        default: 'none',
+
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Spicify the application usage instructions? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the usage instruction!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'creditConfirm',
+        message: 'Are there any other Collaborators?',
+        default: false,
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Specify the Names of other collaborators?',
+        when: ({ creditConfirm }) => creditConfirm
+    },
+    {
+        type: 'confirm',
+        name: 'licenseConfirm',
+        message: 'Do you want to add a spicific license?',
+        default: true,
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Specify the liscence of the application?',
+        choices: ['MIT', 'Apache 2.0', 'Mozilla Public License 2.0'],
+        when: ({ licenseConfirm }) => licenseConfirm,
+    },
+
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Specify the information about the tests?',
+        default: 'none'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your git hub address? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your Github address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'mail',
+        message: 'Enter your E-MAIL address? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the Email!');
+                return false;
+            }
+        }
+    },
+
+
+];
+
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data);
+};
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions).then(responses => {
+        writeToFile("README.md", generateMarkdown(responses));
+    });
+}
 
 // Function call to initialize app
-init();
+init()
+
+
+
+
+
+
+
